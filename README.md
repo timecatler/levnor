333e70bf1f078b581933991916df70e27cda54e5
Tim Catler <timecatler@gmail.com>
2018-05-07 18:32:53 +0300

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # levnor
+Level normalization utility written in Haskell.
+## Features
+Takes image and transforms luminance to a normal distribution.
+Works with grayscale images.
+
+Grayscale input
+![Grayscale in](./images/examples/grayscale.jpg)
+
+Grayscale output
+![Grayscale out](./images/examples/grayscale_out.bmp)
+
+Also works with colored images.
+
+Colored input
+![Colored in](./images/examples/colored.jpg)
+
+Colored output
+![Colored out good](./images/examples/colored_out_good.bmp)
+
+And some distribution parameters work worse than others
+
+Colored output bad
+![Colored out bad](./images/examples/colored_out_bad.bmp)
+
+## Requirements
+* Stack
+
+## Building
+```shell-script
+git clone https://github.com/timecatler/levnor.git
+cd levnor
+stack install
+```
+
+Stack binaries location should be on your `PATH` for that to work.
+
+## Usage
+
+`levnor ./path_to_input_image.jpeg ./path_to_output_image.bmp median standart_deviation`
--- src/Lib.hs
+++ src/Lib.hs
@@ -10,13 +10,11 @@ import           Statistics.Distribution        (quantile)
 import           Statistics.Distribution.Normal
 import           Flow
 
--- Вытаскивает пиксели
 pixels2List :: (Pixel a) => Image a -> [a]
 pixels2List = pixelFold (\pxs _ _ px -> px : pxs) []
 
 extractRawLuminance :: (LumaPlaneExtractable a) => Image a -> [PixelBaseComponent a]
 extractRawLuminance = pixels2List . extractLumaPlane
--- extractRawLuminance (ImageRGB8 img) = img |> extractPixels |> map demotePixel
 
 histForList xs = map (\x -> length $ filter (== x) xs) [0 .. 255]
 
@@ -27,16 +25,14 @@ computePdfLUT xs = xs |> histForList |> map fromIntegral |> normalizeList
 computeCdfLUT ys = ys |> computePdfLUT |> summate
   where
     summate (x : xs) = x : summate' x xs
-      where
-        summate' x (x' : xs) | x >= 1.0    = 1.0 : summate' 1.0 xs
-                             | otherwise = (x + x') : summate' (x + x') xs
-        summate' x []        = [1.0]
+    summate' x (x' : xs) | x >= 1.0  =      1.0 : summate' 1.0      xs
+                         | otherwise = (x + x') : summate' (x + x') xs
+    summate' x []        = [1.0]
 
 getTransformationCoeff :: (Int -> Double) -> Int -> Double
 getTransformationCoeff transformation x | x == 0    = (transformation x)
                                         | otherwise = (transformation x) / (fromIntegral x)
 
--- genTransformation :: Double -> Double -> [PixelF] -> (PixelF -> PixelF)
 genTransformation m s rawLum = inverseSmirnov . directSmirnov
   where
     directSmirnov  = (!!) (computeCdfLUT rawLum)
@@ -44,15 +40,8 @@ genTransformation m s rawLum = inverseSmirnov . directSmirnov
     inverseSmirnov x | x >= 1.0  = quantile (normalDistr m s) 1.0
                      | otherwise = quantile (normalDistr m s) x
 
-normalizeGrayscale :: Double -> Double -> Image Pixel8 -> Image Pixel8
-normalizeGrayscale m s img = pixelMap (correct . genTransformation m s (extractRawLuminance img) . fromIntegral) img
-  where
-    correct x | x < 0     = 0
-              | x > 255   = 255
-              | otherwise = round x
-
-normalizeColored :: Double -> Double -> Image PixelRGB8 -> Image PixelRGB8
-normalizeColored m s img = pixelMapXY (transformWithAt (genTransformation m s (extractRawLuminance img))) img
+normalize :: Double -> Double -> Image PixelRGB8 -> Image PixelRGB8
+normalize m s img = pixelMapXY (transformWithAt (genTransformation m s (extractRawLuminance img))) img
   where
     transformWithAt transformation x y = transformWith (getTransformationCoeff transformation $ fromIntegral $ pixelAt srcLum x y)
     srcLum = extractLumaPlane img
@@ -64,11 +53,9 @@ normalizeColored m s img = pixelMapXY (transformWithAt (genTransformation m s (e
                 | lvl > 255 = 255
                 | otherwise = round lvl
 
-processImage res m s img = writeBitmap res $ (normalizeColored m s . convertRGB8) img
--- transformImage 128 32 (ImageY8 $ )
+processImage res m s img = writeBitmap res $ (normalize m s . convertRGB8) img
 
 someFunc :: IO ()
 someFunc = do
     src:res:m:s:_ <- getArgs
     readImage src >>= either putStrLn (processImage res (read m) (read s))
-
